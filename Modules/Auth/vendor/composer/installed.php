4ef058d096c8926cde8fa847acc557a886138880
jkemboe <jkemboe@gmail.com>
2021-12-28 10:36:56 -0500

    Deleted

---
+++ Modules/Auth/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3fda1811d09755311caaf489b99105a7ac321979',
+        'name' => 'jkemboe/auth',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jkemboe/auth' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3fda1811d09755311caaf489b99105a7ac321979',
+            'dev_requirement' => false,
+        ),
+    ),
+);
