31a4af966a4feca6872ecf14deae92a4ae1d0197
jkemboe <jkemboe@gmail.com>
2021-12-28 08:10:34 -0500

    Register user Data Transfer Object

---
+++ Modules/Auth/DTO/CreateRegisterUserDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Auth\DTO;
+
+class CreateRegisterUserDTO
+{
+    public function __construct(
+        public string $first_name,
+
+        public string $last_name,
+
+        public int $mobile_number,
+
+        public int $identification_number,
+
+        public string $password,
+
+        public string $email
+    )
+    {}
+}
