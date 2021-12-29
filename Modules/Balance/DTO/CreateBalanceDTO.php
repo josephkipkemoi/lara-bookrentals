b03f15f06089fb1164153db6b1d930ce9af8b7dd
jkemboe <jkemboe@gmail.com>
2021-12-29 08:58:17 -0500

    user id field

--- Modules/Balance/DTO/CreateBalanceDTO.php
+++ Modules/Balance/DTO/CreateBalanceDTO.php
@@ -5,7 +5,9 @@ namespace Modules\Balance\DTO;
 class CreateBalanceDTO
 {
     public function __construct(
-        public int $balance
+        public int $balance,
+
+        public int $user_id
     ){}
 }
 
