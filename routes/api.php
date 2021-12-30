cc5b0c0b3a0b73a4b3ad565976d1056473f26ca2
jkemboe <jkemboe@gmail.com>
2021-12-30 16:01:35 -0500

    get roles

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Modules\Balance\Http\Controllers\BalanceController;
+use Modules\Role\Http\Controllers\GetRoleController;
 use Modules\Role\Http\Controllers\RoleController;
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,7 @@ use Modules\Role\Http\Controllers\RoleController;
 
 Route::post("v1/balances", BalanceController::class )->middleware('guest');
 Route::post("v1/roles", RoleController::class )->middleware('guest');
+Route::get("v1/roles", GetRoleController::class )->middleware('guest');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
