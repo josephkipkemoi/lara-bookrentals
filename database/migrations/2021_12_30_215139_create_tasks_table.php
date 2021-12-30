c48f0f763303dce4e773ac56fff3c5b827a43835
jkemboe <jkemboe@gmail.com>
2021-12-30 16:49:33 -0500

    Assignment and Task tables

---
+++ database/migrations/2021_12_30_215139_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('task_id')
+                  ->references('id')
+                  ->on('assignments');
+            $table->boolean('task_completed');
+            $table->time('task_started_at');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
