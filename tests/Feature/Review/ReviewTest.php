4f01c9dd5b8a3f497d689e4b7ea009f484f3b061
jkemboe <jkemboe@gmail.com>
2021-12-31 14:32:04 -0500

    Post review test

---
+++ tests/Feature/Review/ReviewTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\Review;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Modules\Auth\Models\User;
+use Tests\TestCase;
+
+class ReviewTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+    /**
+     * Test user can post review.
+     *
+     * @return void
+     */
+    public function test_user_can_post_review()
+    {
+        $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'identification_number' => 329590355,
+            'mobile_number' => 254700545727,
+            'email' => $this->faker->email(),
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
+
+        $response = $this->post('api/v1/reviews',[
+            'review_title' => 'Good webiste',
+            'review_body' => $this->faker()->text(),
+            'review_rating' => $this->faker()->numberBetween(1,5),
+            'user_id' => $user->id
+        ]);
+
+        $response->assertCreated();
+    }
+}
