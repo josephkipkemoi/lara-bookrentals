ed43bfb4d1fc7b50594cef1a35d7e41a15d785b9
jkemboe <jkemboe@gmail.com>
2021-12-31 14:39:54 -0500

    Get reviews test

--- tests/Feature/Review/ReviewTest.php
+++ tests/Feature/Review/ReviewTest.php
@@ -37,4 +37,28 @@ class ReviewTest extends TestCase
 
         $response->assertCreated();
     }
+
+    public function test_can_get_reviews()
+    {
+        $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'identification_number' => 329590355,
+            'mobile_number' => 254700545727,
+            'email' => $this->faker->email(),
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
+
+        $response = $this->post('api/v1/reviews',[
+            'review_title' => 'Good webiste',
+            'review_body' => $this->faker()->text(),
+            'review_rating' => $this->faker()->numberBetween(1,5),
+            'user_id' => $user->id
+        ]);
+
+        $response = $this->get('api/v1/reviews');
+
+        $response->assertOk();
+    }
 }
