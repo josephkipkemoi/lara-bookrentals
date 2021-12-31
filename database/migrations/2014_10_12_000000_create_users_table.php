f57e31419a8920c923df754a715fc8dbd89914a8
jkemboe <jkemboe@gmail.com>
2021-12-31 15:00:17 -0500

    Verified column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->bigInteger('mobile_number');
             $table->bigInteger('identification_number');
             $table->string('password');
+            $table->boolean('verified')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
