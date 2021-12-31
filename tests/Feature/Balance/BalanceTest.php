d49a5136269643c72d778b988d7b81e9bd4ce871
jkemboe <jkemboe@gmail.com>
2021-12-31 13:28:24 -0500

    Get balance for user

--- tests/Feature/Balance/BalanceTest.php
+++ tests/Feature/Balance/BalanceTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Modules\Auth\Models\User;
 use Tests\TestCase;
 use Illuminate\Support\Str;
+use Modules\Assignment\Models\Assignment;
 use Modules\Balance\Models\Balance;
 
 class BalanceTest extends TestCase
@@ -18,7 +19,7 @@ class BalanceTest extends TestCase
      *
      * @return void
      */
-    public function test_user_can_post_balance()
+    public function test_user_can_increment_balance_after_task_is_complete()
     {
         $user = User::create([
         'first_name' => $this->faker->firstName(),
@@ -39,5 +40,25 @@ class BalanceTest extends TestCase
         ]);
 
         $response->assertCreated();
+    }
+
+    public function test_can_get_user_balance()
+    {
+        $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'identification_number' => 32959035,
+            'mobile_number' => 0700545727,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        Balance::create(['balance' => 0, 'user_id' => $user->id]);
+
+        $response = $this->get("api/v1/balances/$user->id");
+
+        $response->assertOk();
     }
 }
