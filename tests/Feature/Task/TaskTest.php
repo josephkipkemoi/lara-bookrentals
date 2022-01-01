a589f20709cd3303261cb9cb1c3b8c6afe070c4f
jkemboe <jkemboe@gmail.com>
2022-01-01 10:09:04 -0500

    get tasks test

--- tests/Feature/Task/TaskTest.php
+++ tests/Feature/Task/TaskTest.php
@@ -47,11 +47,43 @@ class TaskTest extends TestCase
             'assignment_earning' => $this->faker()->numberBetween(50,100),
          ]);
 
-        $response->assertCreated();
+         $response->assertCreated();
 
         //  Test if task is complete, balance is updated by 10
         $response->getData()->task_completed ?
         assertEquals(Balance::where('user_id', $user->id)->pluck('balance')[0], 10) :
         assertEquals(Balance::where('user_id', $user->id)->pluck('balance')[0], 0);
+    }
+
+    public function test_user_can_get_complete_task_results()
+    {
+        $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'identification_number' => 329590355,
+            'mobile_number' => 254700545727,
+            'email' => $this->faker->email(),
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ]);
+
+        $assignment = Assignment::create([
+            'question' => $this->faker()->text(),
+            'category' => $this->faker()->word()
+        ]);
+
+        $this->post('api/v1/tasks',[
+            'assignment_id' => $assignment->id,
+            'user_id' => $user->id,
+            'task_completed' => $this->faker()->boolean(),
+            'task_completed_at' => $this->faker()->time(),
+            'assignment_category' => $this->faker()->word(),
+            'assignment_rating' => $this->faker()->numberBetween(0,10),
+            'assignment_earning' => $this->faker()->numberBetween(50,100),
+        ]);
+
+        $response = $this->get("api/v1/tasks?user={$user->id}");
+
+        $response->assertOk();
     }
 }
