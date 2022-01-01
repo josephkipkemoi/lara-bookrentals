0f7a5edafa4a0049f9e453f41d5dd805b8a77556
jkemboe <jkemboe@gmail.com>
2022-01-01 10:41:29 -0500

    Test

---
+++ tests/Feature/Admin/AdminTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AdminTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_can_get_all_registered_users()
+    {
+        $response = $this->get('api/v1/admin');
+        dd($response);
+        $response->assertStatus(200);
+    }
+}
