05eb01db6a731524832ee1c9d2fcb9eaf591c097
jkemboe <jkemboe@gmail.com>
2022-01-05 16:05:55 -0500

    Model category

---
+++ Modules/Category/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Category\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category'
+    ];
+
+    protected $casts = [
+        'category' => 'string',
+    ];
+
+}
