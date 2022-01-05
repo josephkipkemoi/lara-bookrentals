291e7032952b23623f01dcb887eb1a6e6cf2eda6
jkemboe <jkemboe@gmail.com>
2022-01-05 16:03:56 -0500

    category test for posting and retreiving categories

---
+++ tests/Feature/Category/CategoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Category;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+    /**
+     * Test can post category
+     *
+     * @return void
+     */
+    public function test_can_post_category()
+    {
+        $response = $this->post('api/v1/categories', [
+            'category' => $this->faker()->word(6),
+        ]);
+
+        $response->assertCreated();
+    }
+
+    public function test_can_get_categories()
+    {
+        $this->post('api/v1/categories', [
+            'category' => $this->faker()->word(6),
+        ]);
+        $this->post('api/v1/categories', [
+            'category' => $this->faker()->word(6),
+        ]);
+
+        $response = $this->get('api/v1/categories');
+
+        $response->assertOk()
+                 ->assertJsonCount(2);
+    }
+}
