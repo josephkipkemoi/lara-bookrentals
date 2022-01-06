162bddd54e5f6fef686996517e0c5c9eabbbbf1c
jkemboe <jkemboe@gmail.com>
2022-01-06 09:16:43 -0500

    Assignment model relationship

--- Modules/Assignment/Models/Assignment.php
+++ Modules/Assignment/Models/Assignment.php
@@ -5,6 +5,7 @@ namespace Modules\Assignment\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Category\Models\Category;
+use Modules\Task\Models\Task;
 
 class Assignment extends Model
 {
@@ -25,5 +26,10 @@ class Assignment extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
     }
 }
