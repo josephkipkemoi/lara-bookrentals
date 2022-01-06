406844bdc078c550a4a72ea84702b55db749318a
jkemboe <jkemboe@gmail.com>
2022-01-06 09:19:25 -0500

    assignment user pivot table

---
+++ database/migrations/2022_01_06_132757_create_assignment_user_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignmentUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignment_user', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users');
+
+            $table->unsignedBigInteger('assignment_id');
+            $table->foreign('assignment_id')
+                  ->references('id')
+                  ->on('assignments');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignment_user');
+    }
+}
