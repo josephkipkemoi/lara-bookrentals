34bcdc0b92f5faa7b5fc30464970c0f48c0a01b6
jkemboe <jkemboe@gmail.com>
2022-01-06 09:18:08 -0500

    Task user relationship

--- Modules/Task/Models/Task.php
+++ Modules/Task/Models/Task.php
@@ -4,6 +4,7 @@ namespace Modules\Task\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Assignment\Models\Assignment;
 use Modules\Auth\Models\User;
 
 class Task extends Model
@@ -37,5 +38,10 @@ class Task extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function assignment()
+    {
+        return $this->hasOne(Assignment::class);
     }
 }
