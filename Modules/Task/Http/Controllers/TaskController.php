0406ad3b6940b34b4dc5112e5819b6e7817263f6
jkemboe <jkemboe@gmail.com>
2022-01-06 09:17:27 -0500

    Task controller mplementaion

--- Modules/Task/Http/Controllers/TaskController.php
+++ Modules/Task/Http/Controllers/TaskController.php
@@ -2,6 +2,8 @@
 
 namespace Modules\Task\Http\Controllers;
 
+use Modules\Assignment\Models\Assignment;
+use Modules\Auth\Models\User;
 use Modules\Task\DTO\CreateTaskDTO;
 use Modules\Task\Http\Requests\CreateTaskRequest;
 use Modules\Task\Models\Task;
@@ -9,8 +11,8 @@ use Modules\Task\Models\Task;
 class TaskController extends Controller
 {
     // create task
-    public function __invoke(CreateTaskRequest $request, Task $task)
+    public function __invoke(CreateTaskRequest $request, User $user, Assignment $assignment)
     {
-      return $task->create((array) new CreateTaskDTO(...$request->validated()));
+      return $user->task()->create((array) new CreateTaskDTO(...$request->validated()));
     }
 }
