ff6f1fe83e67784e0f6c649c33b01e29501b4264
jkemboe <jkemboe@gmail.com>
2022-01-15 07:46:22 -0500

    Role test

--- tests/Feature/Role/RoleTest.php
+++ tests/Feature/Role/RoleTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature\Role;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Modules\Auth\Models\User;
 use Modules\Role\Models\Role;
 use Tests\TestCase;
+use Illuminate\Support\Str;
 
 class RoleTest extends TestCase
 {
@@ -16,25 +18,100 @@ class RoleTest extends TestCase
      *
      * @return void
      */
-    public function test_can_post_role()
+    public function test_admin_can_post_roles()
     {
-        $response = $this->post('api/v1/roles',[
-            'role' => 'manager'
+        $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'identification_number' => 32959035,
+            'mobile_number' => 0700545727,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        $response = $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_ASSISTANT_0
+        ]);
+
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_ADMINISTRATOR_1
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_SPECIALIST_2
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_MANAGER_3
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' =>  Role::IS_EXECUTIVE_4
+        ]);
+
+        $response->assertCreated();
+    }
+
+    public function test_user_can_set_role()
+    {
+
+        $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'identification_number' => 32959035,
+            'mobile_number' => 0700545727,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_ASSISTANT_0
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_ADMINISTRATOR_1
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_SPECIALIST_2
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' => Role::IS_MANAGER_3
+        ]);
+        $this->post("api/v1/{$user->id}/roles",[
+            'role' =>  Role::IS_EXECUTIVE_4
+        ]);
+
+        $roles = Role::get(['role']);
+
+        $response = $this->post("api/v1/{$user->id}/roles", [
+            'role' => $roles[0]->role
         ]);
 
         $response->assertCreated();
     }
 
     // Test client can get roles
-    public function test_can_get_role()
+    public function test_can_get_user_role()
     {
-        Role::create(['role' => 'CEO']);
-        Role::create(['role' => 'Director']);
-        Role::create(['role' => 'Manager']);
+       $user = User::create([
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'identification_number' => 32959035,
+            'mobile_number' => 0700545727,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
 
-        $response = $this->get('api/v1/roles');
+      $role =  Role::create(['role' => Role::IS_ASSISTANT_0]);
 
-        $response->assertOk()
-                 ->assertJsonCount(3);
-    }
+      $this->post("api/v1/{$user->id}/roles", [
+          'role' => Role::IS_ASSISTANT_0
+      ]);
+
+      $response = $this->get("api/v1/{$user->id}/roles/$role->id");
+
+      $response->assertOk();
+     }
 }
