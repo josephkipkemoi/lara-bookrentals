51630c00676402b48ddd97103887611078280adf
jkemboe <jkemboe@gmail.com>
2022-01-15 07:45:42 -0500

    Role relationship

--- Modules/Role/Models/Role.php
+++ Modules/Role/Models/Role.php
@@ -4,6 +4,7 @@ namespace Modules\Role\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Auth\Models\User;
 
 class Role extends Model
 {
@@ -13,6 +14,17 @@ class Role extends Model
      *
      * @var array<int, string>
      */
+
+    public const IS_ASSISTANT_0 = 'Assistant';
+
+    public const IS_ADMINISTRATOR_1 = 'Administrator';
+
+    public const IS_SPECIALIST_2 = 'Specialist';
+
+    public const IS_MANAGER_3 = 'Manager';
+
+    public const IS_EXECUTIVE_4 = 'Executive';
+
     protected $fillable = [
         'role'
     ];
@@ -20,4 +32,9 @@ class Role extends Model
     protected $casts = [
         'role' => 'string',
     ];
+
+    public function user()
+    {
+        return $this->hasMany(User::class);
+    }
 }
