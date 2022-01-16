3957abc2e54599e8aae1c155a9299feed301eac1
jkemboe <jkemboe@gmail.com>
2022-01-16 14:54:02 -0500

    Incomplete mail test

---
+++ tests/Feature/Mail/MailTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature\Mail;
+
+use App\Mail\PaymentProcessed;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Route;
+
+class MailTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_email_is_sent_after_payment_is_made(){
+
+
+        $response = $this->get('api/v1/send-mail');
+
+
+        dd($response);
+    }
+}
